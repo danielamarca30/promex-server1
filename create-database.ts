b48f25aa7c66451c82b72db2f2aad56616fa10f1
danielamarca30 <159821052+danielamarca30@users.noreply.github.com>
2024-11-20 16:57:54 -0400

    Update create-database.ts

--- create-database.ts
+++ create-database.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 async function main() {
-  const host = process.env.DB_HOST || 'localhost';
+  const host = process.env.DB_HOST || 'mysql';
   const user = process.env.DB_USER || 'root';
   const password = process.env.DB_PASSWORD || 'root';
   const dbName = process.env.DB_NAME || 'sistema_colas';
