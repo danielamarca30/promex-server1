e599f30184071b91437a8a5e461fdaae7027c396
danielamarca30 <159821052+danielamarca30@users.noreply.github.com>
2024-11-20 17:00:16 -0400

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,7 +8,7 @@ export default {
   out: './drizzle',
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'mysql',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'sistema_colas',
